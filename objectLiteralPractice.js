5ca867baa83950793a1d422aa8da1d006ae917f6
Adrian Zabolitzki <adrianzab@gmail.com>
2023-04-20 21:34:59 -0600

    add Object person and print object, keys, and values

--- objectLiteralPractice.js
+++ objectLiteralPractice.js
@@ -1,6 +1,17 @@
 var person = {
+    name:  "Zack",
+    age: 18,
     sign: "aries",
-    pet: true,
-    
-
+    pet: true
 }
+
+console.log(person);
+
+console.log(person.name);
+
+person.gender = "male";
+person.hair = "brown";
+
+console.log(person);
+
+console.log(Object.keys(person));
